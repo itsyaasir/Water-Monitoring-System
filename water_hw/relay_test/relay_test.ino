2133f8955497d6f8b960995975f2b7046f5d7ddd
Yasir Shariff <goodwonder5@gmail.com>
2023-03-19 21:13:20 +0300

    Add arduino h/w codes

---
+++ water_hw/relay_test/relay_test.ino
@@ -0,0 +1,17 @@
+int relayInput = 13; // the input to the relay pin
+void setup() {
+
+  pinMode(relayInput, OUTPUT); // initialize pin as OUTPUT
+
+}
+void loop() {
+
+  digitalWrite(relayInput, HIGH); // turn relay on
+
+  delay(1000);
+
+  digitalWrite(relayInput, LOW); // turn relay off
+
+  delay(1000);
+
+}
